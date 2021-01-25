7960b5a89928f0f0bb7f1fbc5c964749072e45c5
Sam Hollyer <rsamhollyer@gmail.com>
2021-01-25 11:39:15 -0600

    stuff

--- src/Details.js
+++ src/Details.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React from "react";
 
-function Details({character}) {
-  return (
-    <div>
-      <h3>{character.name || character.aliases[0]}</h3>
-      <ul>
-        {
-          Object.keys(character).map(key => (
-            key !== 'name' ? <li>{key}: {character[key]}</li> : null
-          ))
-        }
-      </ul>
-    </div>
-  );
+function Details({ character }) {
+	return (
+		<div>
+			<h3>{character.name || character.aliases[0]}</h3>
+			<ul>
+				{Object.entries(character).map(([key, value]) => {
+					return key !== "name" && value && value[0] !== "" ? (
+						<li>
+							{key}: {value}
+						</li>
+					) : null;
+					// value !== null &&
+					// value !== "" &&
+					// value !== undefined &&
+					// value !== 0 &&
+					// value[0] !== ""
+					// ? (
+					// 	<li>
+					// 		{key}: {value}
+					// 	</li>
+					// ) : null;
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default Details;
